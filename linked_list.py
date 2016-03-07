7266f8d0b411cc3e609dc6de5d6c75756548e2e3
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-07 13:56:42 -0800

    draft material

---
+++ linked_list.py
@@ -0,0 +1,19 @@
+class LinkedList(object):
+
+    def insert(val):
+        # insert value at head of list
+        pass
+
+
+class Node(object):
+
+    def __init__(self, data=None, next_node=Node):
+        self.data = data
+
+
+
+
+    def 2()
+        pass
+
+# methods: insert, pop, size, search, remove, display
