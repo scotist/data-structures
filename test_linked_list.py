a31d306243c9bd7a5f8099d4509cfeaeaaba2ecb
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-07 14:34:07 -0800

    one working method, one working test

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,2 +1,9 @@
 # _*_ encoding: utf-8 _*_
 """Test linked_list.py."""
+
+
+def test_new_list():
+    """Test list constructor."""
+    from linked_list import LinkedList
+    new_list = LinkedList()
+    assert isinstance(new_list, LinkedList)
