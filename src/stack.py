4a2a47a296af8cf8cd656bca0400f316e2be9a03
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-08 15:26:39 -0800

    all files fully linted

--- src/stack.py
+++ src/stack.py
@@ -4,17 +4,17 @@ from linked_list import LinkedList
 
 
 class Stack(object):
+    """Make stack object from linked list object."""
 
     def __init__(self):
+        """Initialize stack."""
         self._container = LinkedList()
 
-
     def push(self, val):
+        """Push item to stack."""
         self._container.insert(val)
         self.head = self._container.head
 
-
     def pop(self):
+        """Pop item from stack."""
         return self._container.pop()
-
-
