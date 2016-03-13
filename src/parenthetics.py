ae7574d33796b990647c9a27b93839c932f5aaaf
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-13 00:25:01 -0800

    finished linting

--- src/parenthetics.py
+++ src/parenthetics.py
@@ -1,26 +1,11 @@
 # _*_ encoding: utf-8 _*_
 """Function to check if sets of parentheses in string are well-formed."""
-# from stack import Stack
-
-# test_stack = Stack()
-
-# from collections import deque
-# test_stack = deque()
-
 
 
 def evaluate_parens(sample):
     """Check whether sets of parentheses in string are well-formed."""
-    parens = []
     open_check = 0
     closed_check = 0
-    # balanced = True
-
-    for c in reversed(sample):
-        if c is '(':
-            parens.append(c)
-        elif c is ')':
-            parens.append(c)
 
     for item in sample:
         if item == '(':
@@ -30,42 +15,7 @@ def evaluate_parens(sample):
         if closed_check > open_check:
             return -1
 
-    # if open_check > closed_check:
-    #     return 1
-    # elif closed_check > open_check:
-    #     return -1
     if closed_check == open_check:
         return 0
     else:
         return 1
-
-    # for p in parens:
-    #     test_stack.push(p)
-
-    # for p in parens:
-    #     test_stack.append(p)
-
-    # for item in test_stack:
-    #     if test_stack.pop() == '(':
-    #         counter += 1
-    #     elif test_stack.pop() == ')':
-    #         counter -= 1
-    #     elif IndexError:
-    #         break
-
-  # while True:
-  #     try:
-      #     if test_stack.pop() == '(':
-      #         counter += 1
-      #     elif test_stack.pop() == ')':
-      #         counter -= 1
-      # except IndexError:
-      #     break
-
-    return counter
-
-
-    # split string
-    # filter out everything but parens
-    # make counter
-    # push parens into stack
--- src/test_parenthetics.py
+++ src/test_parenthetics.py
@@ -1,17 +1,6 @@
 # _*_ encoding: utf-8 _*_
 """Test parenthetics.py."""
-import pytest
 
-TEST_CASES = [(['a'], 0),
-              (['()'], 0),
-              (['(Hello))'], -1)]
-
-
-# @pytest.mark.parametrize('li, result', TEST_CASES)
-# def test_evaluate_parens(li, result):
-#     """Test the eval function."""
-#     from parenthetics import evaluate_parens
-#     assert evaluate_parens(li) == result
 
 def test_evaluate_parens_0():
     """Test first case."""
@@ -36,6 +25,8 @@ def test_evaluate_parens_03():
     from parenthetics import evaluate_parens
     assert evaluate_parens('((Hello)') == 1
 
-def test_evaluate_parens():
+
+def test_evaluate_parens_04():
+    """Final test case."""
     from parenthetics import evaluate_parens
     assert evaluate_parens(')))(((') == -1
