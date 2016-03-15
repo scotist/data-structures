0450471009ee7cf02aab136bc12565eedaf99433
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-15 15:00:50 -0700

    got some functionality and tests working

--- src/heap.py
+++ src/heap.py
@@ -1,17 +1,41 @@
 # _*_ encoding: utf-8 _*_
-
+import math
 
 class Heap(object):
     """Make a binary heap."""
 
     def __init__(self, val=None):
-        """Initialize heap."""
-        heap = []
+        """Create heap."""
+        self._container = Heap_Tools(val)
+
 
     def pop():
-        """Pope first item off of heap."""
+        """Pop first item off of heap."""
 
-    def push(val):
+    def push(self, heap, val):
         """Push value into heap."""
+        self.heap.append(val)
+        if val < Heap[len(self.heap) - 1]:
+            self._container.place_finder(val, len(self.heap - 1))
 
-    def heapify(heap)
+
+
+class Heap_Tools(object):
+    """Orders list into a heap."""
+
+    def __init__(self, val):
+        """Initialize heap."""
+        if isinstance(val, list):
+            self.heap = val
+        else:
+            self.heap = []
+
+    def place_finder(self, heap, index):
+        """Puts value in correct position."""
+
+    def parent(self, heap, i):
+        if i == 0:
+            return None
+        return math.floor((i - 1) / 2)
+
+
--- src/test_heap.py
+++ src/test_heap.py
@@ -6,33 +6,44 @@ import math
 heap = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
 
 
-def test_heapify(A, i):
-    assert(False)
+def test_inheritance():
+    """Test init from heap."""
+    from heap import Heap
+    from heap import Heap_Tools
+    new_heap = Heap()
+    assert isinstance(new_heap._container, Heap_Tools)
+
+
+
+# def test_heapify(A, i):
+#     assert(False)
 
 
 def test_root_parent():
-    assert(parent(head, 0) is None)
+    from heap import Heap
+    new_heap = Heap(heap)
+    assert new_heap._container.parent(new_heap, 0) == None
     print("pass")
 
 
-def test_a_parent():
-    assert(False)
+# def test_a_parent():
+#     assert(False)
 
 
-def test_a_left():
-    left(i)
+# def test_a_left():
+#     left(i)
 
 
-def test_a_right():
-    assert(False)
+# def test_a_right():
+#     assert(False)
 
 
-def test_push():
-    n = 11
-    assert(False)
+# def test_push():
+#     n = 11
+#     assert(False)
 
 
-def test_pop():
-    asset(False)
+# def test_pop():
+#     asset(False)
 
 
