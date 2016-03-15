0f8956c7c31eea843d76cffe23ade7779f13bf00
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-15 14:22:27 -0700

    sketched out heap functions

--- src/heap.py
+++ src/heap.py
@@ -1,1 +1,17 @@
 # _*_ encoding: utf-8 _*_
+
+
+class Heap(object):
+    """Make a binary heap."""
+
+    def __init__(self, val=None):
+        """Initialize heap."""
+        heap = []
+
+    def pop():
+        """Pope first item off of heap."""
+
+    def push(val):
+        """Push value into heap."""
+
+    def heapify(heap)
