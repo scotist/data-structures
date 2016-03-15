59eb96f2f984ad28ae0671bb1bd673683ebe6c6c
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-15 14:15:52 -0700

    sketched out tests

---
+++ src/test_heap.py
@@ -0,0 +1,38 @@
+# _*_ encoding: utf-8 _*_
+"""Test heap.py."""
+
+import math
+
+heap = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+
+
+def test_heapify(A, i):
+    assert(False)
+
+
+def test_root_parent():
+    assert(parent(head, 0) is None)
+    print("pass")
+
+
+def test_a_parent():
+    assert(False)
+
+
+def test_a_left():
+    left(i)
+
+
+def test_a_right():
+    assert(False)
+
+
+def test_push():
+    n = 11
+    assert(False)
+
+
+def test_pop():
+    asset(False)
+
+
