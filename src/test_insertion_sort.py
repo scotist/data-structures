69a7d896721b98ffd93f61f6f02156a2e9ab7090
Michael Sullivan <sapientia.veritas@gmail.com>
2016-04-18 20:13:40 -0700

    added test

--- src/test_insertion_sort.py
+++ src/test_insertion_sort.py
@@ -14,3 +14,9 @@ def test_insertion_sort(seq):
     from insertion_sort import insertion_sort
     assert insertion_sort(seq) == sorted(seq)
 
+
+def test_sort_simple():
+    """Test on simple case."""
+    from insertion_sort import insertion_sort
+    a_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    assert insertion_sort(a_list) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
