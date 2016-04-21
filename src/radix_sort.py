86c6aa7bab1ce661acad14c0a195ef9bcd5be08b
Michael Sullivan <sapientia.veritas@gmail.com>
2016-04-21 14:39:50 -0700

    updated with improved time tests

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,6 +1,5 @@
 # _*_ encoding: utf-8 _*_
 """Implement radix sort algorithm in Python."""
-import timeit
 
 
 def radix_sort(a_list):
@@ -17,17 +16,5 @@ def radix_sort(a_list):
 
 
 if __name__ == "__main__":
-    list_one = list(range(990))
-    tries = 10000
-    time1 = timeit.timeit("radix_sort(list_one)", setup="from __main__ import radix_sort, list_one", number=tries)
-    print("""Best case scenario for merge sort algorithm:
-          list one is a list of all the integers from one to ten thousand in ascending order.""".format(time1))
-    print("""Test runs: 10000
-          Average Time = {}""".format(time1 / tries))
-
-    list_two = list(range(990))
-    list_two.reverse()
-    time2 = timeit.timeit("radix_sort(list_one)", setup="from __main__ import radix_sort, list_one", number=tries)
-    print("""In an example of the worst case scenario, list two is the reverse of list one: ten thousand to zero in descending order.""" )
-    print("""Test runs: 10000
-          Average Time = {}""".format(time2 / tries))
+    from time_sorting import time_sorting
+    time_sorting(radix_sort)
