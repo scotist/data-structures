af740f448ee058d7e4994853d8bb3619edbee6e8
Michael Sullivan <sapientia.veritas@gmail.com>
2016-04-21 14:18:13 -0700

    implemented radix sort algorithm, modified previous sort tests appropriately, all passing

---
+++ src/radix_sort.py
@@ -0,0 +1,15 @@
+# _*_ encoding: utf-8 _*_
+"""Implement radix sort algorithm in Python."""
+
+
+def radix_sort(a_list):
+    """Radix sort for positive numbers up to one hundred thousand."""
+    buckets = [[] for x in range(10)]
+    for digit in range(0, 5):
+        for number in a_list:
+            buckets[number // 10 ** digit % 10].append(number)
+        del a_list[:]
+        for bucket in buckets:
+            a_list.extend(bucket)
+            del bucket[:]
+    return a_list
